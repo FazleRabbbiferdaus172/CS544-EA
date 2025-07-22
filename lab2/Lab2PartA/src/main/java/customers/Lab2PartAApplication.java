eaec13e678cc0b7d7d3a25378b3c8e6cc478af0a
FazleRabbbiferdaus172 <fazle.ferdaus1416@gmail.com>
2025-07-22 13:53:37 -0500

    spring boot application setup

--- lab2/Lab2PartA/src/main/java/customers/Lab2PartAApplication.java
+++ lab2/Lab2PartA/src/main/java/customers/Lab2PartAApplication.java
@@ -1,13 +1,21 @@
 package customers;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.SQLOutput;
+
 @SpringBootApplication
-public class Lab2PartAApplication {
+public class Lab2PartAApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Lab2PartAApplication.class, args);
+	}
+
+	@Override
+	public void run(String[] args) throws Exception{
+		System.out.println("ok");
 	}
 
 }
