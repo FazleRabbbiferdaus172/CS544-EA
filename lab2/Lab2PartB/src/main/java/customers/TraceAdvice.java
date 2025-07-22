ce5dab14248efbd50df40eab01da2c4611529a33
FazleRabbbiferdaus172 <fazle.ferdaus1416@gmail.com>
2025-07-22 15:03:23 -0500

    outgoing mail server is now logged in trace advice

--- lab2/Lab2PartB/src/main/java/customers/TraceAdvice.java
+++ lab2/Lab2PartB/src/main/java/customers/TraceAdvice.java
@@ -13,6 +13,6 @@ public class TraceAdvice {
     @After("execution(* customers.EmailSender.sendEmail(..)) && args(email, message)")
     public void traceAfterMethod(JoinPoint joinPoint, String email, String message) {
         System.out.println(LocalDateTime.now()+" method="+joinPoint.getSignature().getName()+ " address="+email+"\n" +
-                "message= "+message);
+                "message= "+message+" \n outgoing mail server="+((EmailSender)joinPoint.getTarget()).outgoingMailServer);
     }
 }
