a98bda03a5eaab944bad0a1d6f06ccd186d6149e
FazleRabbbiferdaus172 <fazle.ferdaus1416@gmail.com>
2025-08-04 15:01:42 -0500

    lab11partD done

--- lab11/Lab11PartB/src/test/java/BooksRESTTest.java
+++ lab11/Lab11PartB/src/test/java/BooksRESTTest.java
@@ -17,6 +17,19 @@ public class BooksRESTTest {
     }
 
     @Test
+    public void testAddBook() {
+        Book book = new Book("999", "A Brand New Book", 34.95, "New Author");
+
+        given()
+                .contentType(ContentType.JSON).body(book)
+                .when()
+                .post("/books")
+                .then()
+                .statusCode(200)
+                .body("isbn", equalTo("999"));
+    }
+
+    @Test
     public void testGetOneBook() {
         // add the book to be fetched
         Book book = new Book("878","Book 123", 18.95, "Joe Smith");
@@ -40,6 +53,82 @@ public class BooksRESTTest {
         given()
                 .when()
                 .delete("books/878");
+    }
+
+    @Test
+    public void testUpdateBook() {
+        Book originalBook = new Book("767", "Original Title", 50.00, "Original Author");
+        given()
+                .contentType(ContentType.JSON)
+                .body(originalBook)
+                .when()
+                .post("/books")
+                .then()
+                .statusCode(200);
+        Book updatedBook = new Book("767", "Updated Title", 55.50, "Updated Author");
+        given()
+                .contentType(ContentType.JSON)
+                .body(updatedBook)
+                .when()
+                .put("books/767")
+                .then()
+                .statusCode(200)
+                .body("title", equalTo(updatedBook.getTitle()))
+                .body("price", equalTo(55.50f));
+        given()
+                .when()
+                .delete("/books/767");
+    }
+
+    @Test
+    public void testDeleteBook() {
+        Book book = new Book("454", "Another Book", 20.00, "Some Author");
+        given()
+                .contentType(ContentType.JSON)
+                .body(book)
+                .when()
+                .post("/books")
+                .then()
+                .statusCode(200);
+        given()
+                .when()
+                .delete("/books/454")
+                .then()
+                .statusCode(204);
+        given()
+                .when()
+                .get("/books/454")
+                .then()
+                .statusCode(404);
+    }
+
+    @Test
+    public void testSearchBooksByAuthor() {
+        Book book = new Book("323", "A Searchable Book", 15.00, "Searchable Author");
+        given()
+                .contentType(ContentType.JSON).body(book)
+                .when().post("/books")
+                .then().statusCode(200);
+
+        given()
+                .when()
+                .get("/books?author=\"Searchable Author\"")
+                .then()
+                .statusCode(200)
+                .body("books[0].isbn", equalTo(book.getIsbn()));
+        given()
+                .when().delete();
+    }
+
+    @Test
+    public void testGetAllBooks() {
+        given()
+                .when()
+                .get("/books")
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("books", instanceOf(java.util.List.class));
     }
 
 
