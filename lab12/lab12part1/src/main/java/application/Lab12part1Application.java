9c83fcb118c2da6d44ccd43edeaf87e220eb836d
FazleRabbbiferdaus172 <fazle.ferdaus1416@gmail.com>
2025-08-05 22:22:41 -0500

    intial commit

---
+++ lab12/lab12part1/src/main/java/application/Lab12part1Application.java
@@ -0,0 +1,13 @@
+package application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab12part1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab12part1Application.class, args);
+	}
+
+}
